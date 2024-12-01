8186b8089ba0566e2a375c1c8f78cc3a35f767d2
seyoner <usupovismail613@gmail.com>
2024-12-01 11:26:01 +0300

    Созданные черновики

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
     path('posts/<int:pk>', views.post_detail, name='post_detail'),
     path('posts/new/', views.post_new, name='post_new'),
-    path('posts/<int:pk</edit/', views.post_edit, name='post_detail'),
+    path('posts/<int:pk>/edit/', views.post_edit, name='post_detail'),
     path('products', views.product_detail, name='product_detail'),
     path('about/', views.about, name='about_us'),
     path('contacts/', views.get_contacts, name='get_contacts'),
-    path('profile/', views.get_profile, name='profile')
+    path('profile/', views.get_profile, name='profile'),
+    path('drafts/', views.post_draft, name='post_draft'),
+    path('post/<int:pk>/info/', views.post_info, name='post_info'),
+    path('posts/<int:pk>/publish/', views.post_publish, name='post_publish'),
 ]
